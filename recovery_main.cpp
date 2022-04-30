4746a99ff31547da9048a8a0efe8a8f9528e6e7c
Joker-V2 <nipinna0@gmail.com>
2022-04-30 07:59:17 +0530

    recovery: allow formatting & mounting system on user builds
    - Needed to flash MicroG
    
    This reverts commit 4fe5c71f9f92b4558b6a929cb6afa33edbacdc18.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- recovery_main.cpp
+++ recovery_main.cpp
@@ -494,12 +494,6 @@ int main(int argc, char** argv) {
     device->RemoveMenuItemForAction(Device::ENTER_RESCUE);
   }
 
-  if (get_build_type() == "user") {
-    device->RemoveMenuItemForAction(Device::WIPE_SYSTEM);
-    device->RemoveMenuItemForAction(Device::MOUNT_SYSTEM);
-    device->RemoveMenuItemForAction(Device::ENABLE_ADB);
-  }
-
   if (!android::base::GetBoolProperty("ro.build.ab_update", false)) {
     device->RemoveMenuItemForAction(Device::SWAP_SLOT);
   }
